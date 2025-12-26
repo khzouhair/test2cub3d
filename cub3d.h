a01dca4c38f093d45603412b8c769c4789f1692a
Khadija Zouhair <147002128+khzouhair@users.noreply.github.com>
2025-12-26 04:10:50 +0100

    Clean up cub3d.h by removing comments
    Removed commented-out code and unnecessary comments from cub3d.h.

--- cub3d.h
+++ cub3d.h
@@ -9,15 +9,6 @@
 #include <sys/types.h>
 #define ROT_SPEED 0.01
 #define PI 3.14159
-/*
-	you should think of this file as a four part header...
-	one for map, player, textures and colors...
-	one for mlx(i'm also using textures here)...
-	one for raycasting...
-	and functions declarations
-	input struct
-*/
-
 
 typedef struct s_xy{
     double	dx;
@@ -41,29 +32,6 @@ typedef struct s_r{
 	int draw_end;
 } t_r;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-		map
-		textures
-		floor and ceiling colors
-		player variables
-*/
-
 typedef struct s_player
 {
 	double x;
@@ -72,14 +40,6 @@ typedef struct s_player
 	double dir_x;
 	double dir_y;
 }   t_player;
-
-// typedef struct s_map
-// {
-//     char    **grid;
-//     int     width;
-//     int     height;
-// }   t_map;
-
 
 typedef struct s_textures
 {
@@ -92,23 +52,12 @@ typedef struct s_textures
 	int		endian;
 }   t_textures;
 
-// typedef struct s_colors
-// {
-// 	int floor;
-//     int ceiling;
-// }   t_colors;
-
-
 typedef struct s_data
 {
 	t_map       map;
-	// t_textures		textures;
-	// t_colors    colors;
 	t_player    player;
 }   t_data;
 
-
-/*mlx:*/
 typedef struct s_img
 {
 	void    *img;
@@ -135,7 +84,6 @@ typedef struct s_wall
     t_tex    *west;
 } t_wall;
 
-/*ray_casting struct*/
 typedef struct s_ray
 {
     double  dir_x;
@@ -152,29 +100,6 @@ typedef struct s_ray
 }   t_ray;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*input*/
 typedef struct s_input
 {
@@ -186,27 +111,29 @@ typedef struct s_input
 	int rotate_right;
 }	t_input;
 
-
-
-
-
 typedef struct s_cub3d
 {
-	t_data	data;   // map, textures, colors, player
-	t_mlx	mlx;    // mlx-related stuff
-	t_ray	ray;    // raycasting data
-	t_input	input;  // user input
+	t_data	data;   
+	t_mlx	mlx;    
+	t_ray	ray;    
+	t_input	input;  
 	t_wall  *wall;
 }	t_cub3d;
 
-/*functions:*/
+
+
+void ft_move_forward(t_xy *c);
+void ft_move_backward(t_xy *c);
+void ft_move_left(t_xy *c);
+void ft_move_right(t_xy *c);
+void ft_type_move(t_input *input, t_xy *c);
+void ft_cal_step_initial_side_distance(t_r *r, t_cub3d *cub3d);
+void ft_init_r(t_r *r, t_cub3d *cub3d);
+void ft_perform(t_r *r, t_cub3d *cub3d);
+void ft_condition_side(t_r *r, t_cub3d *cub3d);
 int 	init_mlx(t_mlx *mlx_data);
 t_data 	init_data(t_mlx *mlx_data, t_map *map);
-// void 	dda(t_mlx *mlx_data, t_data *data, t_img middle_img_buffer);
 void dda(t_cub3d *cub3d);
-// void vertical_line(t_img img, int x, int draw_start, int draw_end, int wall_color, int ceiling_color, int floor_color);
-// void vertical_line(t_cub3d *cub, int x, int draw_start, int draw_end, double wall_x);
-
 int	key_analysis(int keycode, t_input *input);
 void handle_rotation(t_player *player, t_input *input);
 t_input	init_input(void);
@@ -214,14 +141,12 @@ t_img create_img(t_mlx *mlx_data);
 void	clean_image(t_mlx *mlx_data);
 void update_player_position(t_player *player, t_input *input, char **map);
 void reset_input(t_input *input);
-
-//-------------->textures
 t_wall	*init_text(t_cub3d *cub3d);
 void draw_textured_column(t_cub3d *cub3d, t_tex *tex,
                           int column, int y_top, int y_bottom, int *y, double wall_x);
 int get_texture_color(t_tex *tex, int x, int y);
 void	write_pixel(t_cub3d *cub3d, int x, int y, int color);
 void vertical_line(t_cub3d *cub, t_ray *ray, int x, int draw_start, int draw_end, double wall_x);
-// void vertical_line(t_cub3d *cub, int x, int draw_start, int draw_end,
-//                    double wall_x, t_ray *ray);
+
+
 #endif
