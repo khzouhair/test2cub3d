4a759074d282aa50ce8e6ab85b70d4b942ceeddf
Khadija Zouhair <147002128+khzouhair@users.noreply.github.com>
2025-12-26 01:56:40 +0100

    Refactor DDA algorithm into structured functions

--- dda.c
+++ dda.c
@@ -1,91 +1,113 @@
 #include "cub3d.h"
 #include <math.h>
 
-void dda(t_cub3d *cub3d)
-{
+typedef struct s_r{
 	double camera_step;
 	double start_angle;
+	int x;
 	t_ray ray;
 	t_tex *current_tex;
-	camera_step = (double)1.074 / (double)cub3d->mlx.win_width;
-	start_angle = cub3d->data.player.angle - (1.074 / 2);
+	double ray_angle;
+	int map_x;
+	int map_y;
+	double wall_x;
+	int line_height;
+	int draw_start;
+	int draw_end;
+} t_r;
 
-	int x = -1;
-	while (++x < cub3d->mlx.win_width)
-	{
-		double ray_angle = start_angle + x * camera_step;
-		
-		ray.dir_x = cos(ray_angle);
-		ray.dir_y = sin(ray_angle);
-		
-		int map_x = (int)cub3d->data.player.x;
-		int map_y = (int)cub3d->data.player.y;
-		
-		ray.delta_dist_x = fabs(1 / ray.dir_x);
-		ray.delta_dist_y = fabs(1 / ray.dir_y);
-		
-		// Calculate step and initial side distances
-		if (ray.dir_x < 0)
+
+void ft_cal_step_initial_side_distance(t_r *r, t_cub3d *cub3d)
+{
+	if (r->ray.dir_x < 0)
 		{
-			ray.step_x = -1;
-			ray.side_dist_x = (cub3d->data.player.x - map_x) * ray.delta_dist_x;
+			r->ray.step_x = -1;
+			r->ray.side_dist_x = (cub3d->data.player.x - r->map_x) * r->ray.delta_dist_x;
 		}
 		else
 		{
-			ray.step_x = 1;
-			ray.side_dist_x = (map_x + 1.0 - cub3d->data.player.x) * ray.delta_dist_x;
+			r->ray.step_x = 1;
+			r->ray.side_dist_x = (r->map_x + 1.0 - cub3d->data.player.x) * r->ray.delta_dist_x;
 		}
 		
-		if (ray.dir_y < 0)
+		if (r->ray.dir_y < 0)
 		{
-			ray.step_y = -1;
-			ray.side_dist_y = (cub3d->data.player.y - map_y) * ray.delta_dist_y;
+			r->ray.step_y = -1;
+			r->ray.side_dist_y = (cub3d->data.player.y - r->map_y) * r->ray.delta_dist_y;
 		}
 		else
 		{
-			ray.step_y = 1;
-			ray.side_dist_y = (map_y + 1.0 - cub3d->data.player.y) * ray.delta_dist_y;
+			r->ray.step_y = 1;
+			r->ray.side_dist_y = (r->map_y + 1.0 - cub3d->data.player.y) * r->ray.delta_dist_y;
 		}
-		
-		// Perform DDA
-		ray.hit = 0;
-		while (ray.hit == 0)
+}
+
+void ft_init_r(t_r *r, t_cub3d *cub3d)
+{
+		r->ray_angle = r->start_angle + r->x * r->camera_step;
+		r->ray.dir_x = cos(r->ray_angle);
+		r->ray.dir_y = sin(r->ray_angle);
+		r->map_x = (int)cub3d->data.player.x;
+		r->map_y = (int)cub3d->data.player.y;
+		r->ray.delta_dist_x = fabs(1 / r->ray.dir_x);
+		r->ray.delta_dist_y = fabs(1 / r->ray.dir_y);
+}
+
+void ft_perform(t_r *r, t_cub3d *cub3d)
+{
+	while (r->ray.hit == 0)
 		{
-			if (ray.side_dist_x < ray.side_dist_y)
+			if (r->ray.side_dist_x < r->ray.side_dist_y)
 			{
-				ray.side_dist_x += ray.delta_dist_x;
-				map_x += ray.step_x;
-				ray.side = 0;
+				r->ray.side_dist_x += r->ray.delta_dist_x;
+				r->map_x += r->ray.step_x;
+				r->ray.side = 0;
 			}
 			else
 			{
-				ray.side_dist_y += ray.delta_dist_y;
-				map_y += ray.step_y;
-				ray.side = 1;
+				r->ray.side_dist_y += r->ray.delta_dist_y;
+				r->map_y += r->ray.step_y;
+				r->ray.side = 1;
 			}
 			/*here*/
-			if (cub3d->data.map.map[map_y][map_x] == '1')
-				ray.hit = 1;
+			if (cub3d->data.map.map[r->map_y][r->map_x] == '1')
+				r->ray.hit = 1;
 		}
-		/* after you determine ray.side, map_x, map_y and set ray.hit = 1 */
-			/* inside dda loop, after DDA found map_x,map_y and ray.side & ray.step_x/step_y */
-    if (ray.side == 0)
-        ray.perp_wall_dist = (map_x - cub3d->data.player.x + (1 - ray.step_x) / 2.0) / ray.dir_x;
-    else
-        ray.perp_wall_dist = (map_y - cub3d->data.player.y + (1 - ray.step_y) / 2.0) / ray.dir_y;
+}
 
-	// printf("perp:%f\n", ray.perp_wall_dist);
-    ray.perp_wall_dist *= cos(cub3d->data.player.angle - ray_angle);
-    double wall_x;
-    if (ray.side == 0)
-        wall_x = cub3d->data.player.y + ray.perp_wall_dist * ray.dir_y;
+void ft_condition_side(t_r *r, t_cub3d *cub3d)
+{
+	if (r->ray.side == 0)
+        r->ray.perp_wall_dist = (r->map_x - cub3d->data.player.x + (1 - r->ray.step_x) / 2.0) / r->ray.dir_x;
     else
-        wall_x = cub3d->data.player.x + ray.perp_wall_dist * ray.dir_x;
-    wall_x -= floor(wall_x);
-    int line_height = (int)(cub3d->mlx.win_height / ray.perp_wall_dist);
-    int draw_start = cub3d->mlx.win_height / 2 - (line_height / 2);
-    int draw_end   = cub3d->mlx.win_height / 2 + (line_height / 2);
-    vertical_line(cub3d, &ray, x, draw_start, draw_end, wall_x);
+        r->ray.perp_wall_dist = (r->map_y - cub3d->data.player.y + (1 - r->ray.step_y) / 2.0) / r->ray.dir_y;
+
+}
+
+void dda(t_cub3d *cub3d)
+{
+	t_r r;
+
+	r.camera_step = (double)1.074 / (double)cub3d->mlx.win_width;
+	r.start_angle = cub3d->data.player.angle - (1.074 / 2);
+	r.x = -1;
+	while (++r.x < cub3d->mlx.win_width)
+	{
+		ft_init_r(&r, cub3d);
+		ft_cal_step_initial_side_distance(&r, cub3d);
+		r.ray.hit = 0;
+		ft_peform(&r, cub3d);
+    ft_condition_side(&r, cub3d);
+    r.ray.perp_wall_dist *= cos(cub3d->data.player.angle - r.ray_angle);
+    if (r.ray.side == 0)
+        r.wall_x = cub3d->data.player.y + r.ray.perp_wall_dist * r.ray.dir_y;
+    else
+        r.wall_x = cub3d->data.player.x + r.ray.perp_wall_dist * r.ray.dir_x;
+    r.wall_x -= floor(r.wall_x);
+    r.line_height = (int)(cub3d->mlx.win_height / r.ray.perp_wall_dist);
+    r.draw_start = cub3d->mlx.win_height / 2 - (r.line_height / 2);
+    r.draw_end   = cub3d->mlx.win_height / 2 + (r.line_height / 2);
+    vertical_line(cub3d, &r.ray, r.x, r.draw_start, r.draw_end, r.wall_x);
 	}
 }
 
